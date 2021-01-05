d8099c1b64f7261090c7f01f54f44bf947c03283
Aleksandr Bushuev <aleksandr.bushuev2000@gmail.com>
2021-01-05 23:27:39 +0300

    Add support for live example

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,10 +1,16 @@
 const path = require('path');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'production',
     plugins: [
         new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            inject: true,
+            minify: true,
+        }),
     ],
     entry: "./src/index.js",
     output: {
